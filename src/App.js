bb72425fd28e30b8774df7893bc0f32acb4750b7
Anna Buts <annabuts614@gmail.com>
2023-02-18 16:32:34 +0200

    Base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Posts} from "./components/Posts/Posts";
 
 function App() {
   return (
     <div className="App">
-      test
+       <Posts/>
     </div>
   );
 }
